333d0104f73f5a05fcb3cd5ed1887c29e42ac130
Kaizyn <2291443901@qq.com>
2021-05-06 23:23:12 +0800

    finish code, debug is continued

--- 杭电2021程序设计课程实践/4/my.cpp
+++ 杭电2021程序设计课程实践/4/my.cpp
@@ -1,11 +1,12 @@
 /*
  * @Author: Kaizyn
  * @Date: 2021-05-06 19:33:47
- * @LastEditTime: 2021-05-06 22:41:12
+ * @LastEditTime: 2021-05-06 23:21:35
  */
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e2+7;
+// const int N = 1e2+7;
+const int M = 13;
 
 template <typename U>
 class Martix {
@@ -89,11 +90,14 @@ class Martix {
 };
 
 int dataNum;               //样本数目
-double attri[13][N], price[N], min_in[13], max_in[13], min_out, max_out, r[13];
-Martix<double> w; // w = (X'X)^(-1)X'y
-int choose[13];
-string Attribute_name[14];
-// "CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV"
+// double attri[M][N], price[N], min_in[M], max_in[M], min_out, max_out, r[M];
+vector<vector<double>> attri(M);
+vector<double> price, min_in(M), max_in(M), r(M);
+double min_out, max_out;
+Martix<double> W; // w = (X'X)^(-1)X'y
+Martix<double> X, Y, FY;
+vector<int> choose(M);
+vector<string> Attribute_name(M+1);
 
 //数据读入
 bool Inputs() {
@@ -102,29 +106,34 @@ bool Inputs() {
   cin >> fname;
   ifstream fp(fname);
   if (!fp.is_open()) return cout << "不能打开输入的文件\n", false;
-  for (int i = 0; i < 14; ++i) fp >> Attribute_name[i];
+  for (int i = 0; i < M+1; ++i) fp >> Attribute_name[i];
   for (int i = 0; !fp.eof(); ++i, ++dataNum) {
-    for (int j = 0; j < 13; ++j) fp >> attri[j][i];
-    fp >> price[i];
+    double data;
+    for (int j = 0; j < M; ++j) {
+      fp >> data;
+      attri[j].emplace_back(data);
+    }
+    fp >> data;
+    price.emplace_back(data);
   }
   fp.close();
   return true;
 }
 
 //使用最大最小归一化数据
-void Normalization(double x[], double &min_val, double &max_val) {
+void Normalization(vector<double> &x, double &min_val, double &max_val) {
   min_val = max_val = x[0];
-  for (int i = 1; i < dataNum; ++i) {
-    min_val = min(min_val, x[i]);
-    max_val = max(max_val, x[i]);
+  for (auto &v : x) {
+    min_val = min(min_val, v);
+    max_val = max(max_val, v);
   }
-  for (int i = 0; i < dataNum; ++i) {
-    x[i] = (x[i]-min_val)/(max_val-min_val);
+  for (auto &v : x) {
+    v = (v-min_val)/(max_val-min_val);
   }
 }
 
 // 计算相关系数
-double Correlation_coefficient(double x[], double y[]) {
+double Correlation_coefficient(vector<double> &x, vector<double> &y) {
   double ave_x = 0, ave_y = 0, var_x = 0, var_y = 0, cov = 0;
   for (int i = 0; i < dataNum; ++i) {
     ave_x += x[i];
@@ -143,50 +152,63 @@ double Correlation_coefficient(double x[], double y[]) {
 //线性回归
 void LineReg() {
   // w = (X'X)^(-1)X'y
-  Martix<double> X(dataNum, 5), y(dataNum, 1);
+  X = Martix<double>(dataNum, 5);
+  Y = Martix<double>(dataNum, 1);
   for (int i = 0; i < dataNum; ++i) {
     for (int j = 0; j < 4; ++j) {
       X[i][j] = attri[i][choose[j]];
     }
     X[i][4] = 1;
-    y[i][0] = price[i];
+    Y[i][0] = price[i];
   }
-  w = inv(X.T()*X)*X.T()*y;
+  W = inv(X.T()*X)*X.T()*Y;
 }
 
-double Predict(double inx[4]) {
-  Martix<double> x(5, 1);
-  for (int i = 0; i < 4; ++i) x[i][0] = inx[i];
-  x[0][4] = 1;
-  return (w*x)[0][0];
+double MSE() {
+  double mse = 0;
+  FY = X.T()*W;
+  for (int i = 0; i < dataNum; ++i) {
+    mse += (FY[i][0]-Y[i][0])*(FY[i][0]-Y[i][0]);
+  }
+  return mse/dataNum;
 }
 
 //输出
 void Outputs() {
-  static double x[4], y;
-  for (int i = 0; i < 4; ++i) {
-    cout << "请输入房屋的" << Attribute_name[choose[i]] << "属性值:";
-    cin >> x[i];
+  Martix<double> x(5, 1);
+  for (int i = 0, j; i < 4; ++i) {
+    j = choose[i];
+    cout << "请输入房屋的" << Attribute_name[j] << "属性值:";
+    cin >> x[i][0];
+    // 归一化
+    x[i][0] = (x[i][0]-min_in[j])/(max_in[j]-min_in[j]);
   }
-  y = Predict(x);
+  x[4][0] = 1;
+  double y = (W*x)[0][0];
+  y = y*(max_out-min_out)+min_out;
   cout << "预计房价为：" << y << "元\n";
 }
 
 int main() {
   if (!Inputs()) return 0;    //判断数据样本读入是否成功
   // 归一化
-  for (int i = 0; i < 13; ++i) Normalization(attri[i], min_in[i], max_in[i]);
+  for (int i = 0; i < M; ++i) Normalization(attri[i], min_in[i], max_in[i]);
   Normalization(price, min_out, max_out);
-  // 相关系数
-  for (int i = 0; i < 13; ++i) r[i] = Correlation_coefficient(attri[i], price);
+  // 计算相关系数
+  for (int i = 0; i < M; ++i) r[i] = Correlation_coefficient(attri[i], price);
   // 挑选四个最相关的维度
-  iota(choose, choose+13, 0);
-  sort(choose, choose+13, [&](int x, int y) {
+  iota(choose.begin(), choose.end(), 0);
+  sort(choose.begin(), choose.end(), [&](int x, int y) {
     return r[x] > r[y];
   });
   // 多元线性回归
   LineReg();
+  // 模型评估
+  cout << "MSE=" << MSE() << '\n';
   Outputs();
   system("pause");
   return 0;
 }
+/*
+C:\Users\Kaizyn\Documents\MySmallProjects\杭电2021程序设计课程实践\4\housing.txt
+*/
