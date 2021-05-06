65b04894198d071eb8aa8f504316d144c03b8ebc
Kaizyn <2291443901@qq.com>
2021-05-06 22:55:22 +0800

    多元线性回归

---
+++ 杭电2021数电/Q-M/qm.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool comp(int n, char *a, char *b) {
+  for (int i = 0; i < n; ++i) {
+    if (a[i] != b[i] && (a[i] != '-' && b[i] != '-')) return false;
+  }
+  return true;
+}
+
+int implicant(int n, char *a, char *b) {
+  int count = 0, temp;
+  for (int i = 0; i < n; ++i) {
+    if (a[i] != b[i]) {
+      count++;
+      temp = i;
+    }
+  }
+  if (count == 1) return temp;
+  else return -1;
+}
+
+bool cointain(int n, char *a, char *b) {
+  for (int i = 0; i < n; ++i) {
+    if (a[i] != b[i] && (a[i] != '-')) return false;
+  }
+  return true;
+}
+
+signed main() {
+  int n, count, tmp;
+  char *temp;
+  bool flag[2];
+  vector<char*> v[10];
+  vector<char*> relative;
+  vector<char*> prime;
+  vector<char*> result;
+
+  cout << "输入变量数：\n";
+  cin >> n;
+
+  temp = new char[n];
+  cout << "请输入最小项(2结束)\n";
+  flag[0] = true;
+  while (flag[0]) {
+    for (int i = 0; i < n; ++i) {
+      cin >> temp[i];
+      if (temp[i] != '0' && temp[i] != '1') {
+        flag[0] = false;
+        break;
+      }
+    }
+    if (flag[0]) {
+      v[0].emplace_back(new char[n]);
+      relative.emplace_back(new char[n]);
+      strcpy(v[0].back(), temp);
+      strcpy(relative.back(), temp);
+    }
+  }
+  cout << "请输入无关项(2结束)\n";
+  flag[0] = true;
+  while (flag[0]) {
+    for (int i = 0; i < n; ++i) {
+      cin >> temp[i];
+      if (temp[i] != '0' && temp[i] != '1') {
+        flag[0] = false;
+        break;
+      }
+    }
+    if (flag[0]) {
+      v[0].emplace_back(new char[n]);
+      strcpy(v[0].back(), temp);
+    }
+  }
+  for (int i = 0; i < 10; ++i) {
+    if (v[i].empty()) break;
+    for (int j = 0; j < (int)v[i].size(); ++j) {
+      flag[0] = false;
+      for (int k = 0; k < (int)v[i].size(); ++k) {
+        if (implicant(n ,v[i][j], v[i][k]) != -1) {
+          strcpy(temp, v[i][j]);
+          temp[implicant(n, v[i][j], v[i][k])] = '-';
+          flag[1] = true;
+          flag[0] = true;
+          for (int l = 0; l < (int)v[i+1].size(); ++l) {
+            if (!strcmp(v[i+1][l], temp)) flag[1] = false;
+          }
+          if (flag[1]) {
+            v[i+1].emplace_back(new char[n]);
+            strcpy(v[i+1].back(), temp);
+          }
+        }
+      }
+      if (!flag[0]) {
+        prime.emplace_back(new char[n]);
+        strcpy(prime.back(), v[i][j]);
+      }
+    }
+  }
+  for (int i = 0; i < (int)relative.size() && !relative.empty(); ++i) {
+    count = 0;
+    for (int j = 0; j < (int)prime.size() && !prime.empty(); ++j) {
+      if (comp(n, relative[i], prime[j])) {
+        count++;
+        tmp = j;
+      }
+    }
+    if (count == 1) {
+      result.emplace_back(new char[n]);
+      strcpy(result.back(), prime[tmp]);
+      for (int j = 0; j < (int)relative.size(); ++j) {
+        if (comp(n, relative[j], prime[tmp])) {
+          relative.erase(relative.begin()+j);
+          --j;
+        }
+      }
+      prime.erase(prime.begin()+tmp);
+      --i;
+    }
+  }
+  while (!relative.empty()) {
+    strcpy(temp, prime.back());
+    prime.pop_back();
+    count = 0;
+    for (int i = 0; i < (int)relative.size(); ++i) {
+      if (comp(n, relative[i], temp)) {
+        relative.erase(relative.begin()+i);
+        --i;
+        ++count;
+      }
+    }
+    if (count > 0) {
+      result.emplace_back(new char[n]);
+      strcpy(result.back(), temp);
+    }
+  }
+  while (result.size()) {
+    for (int i = 0; i < 4; ++i) {
+      cout << *(result.back()+i);
+    }
+    cout << ' ';
+    result.pop_back();
+  }
+  cout << '\n';
+  system("pause");
+  return 0;
+}
+/*
+4
+0100
+0101
+0110
+1000
+1001
+1010
+1101
+2
+0000
+1111
+0111
+2
+
+1-01 -1-1 10-0 01--
+*/
--- 杭电2021程序设计课程实践/4/my.cpp
+++ 杭电2021程序设计课程实践/4/my.cpp
@@ -1,15 +1,96 @@
 /*
  * @Author: Kaizyn
  * @Date: 2021-05-06 19:33:47
- * @LastEditTime: 2021-05-06 20:51:45
+ * @LastEditTime: 2021-05-06 22:41:12
  */
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 1e2+7;
 
+template <typename U>
+class Martix {
+ public:
+  Martix() {}
+  Martix(const int &_n, const int &_m = 1) : n(_n), m(_m) {
+    a = vector<vector<U>>(n, vector<U>(m, 0));
+  }
+  bool empty() const { return n == 0 || m == 0; }
+  vector<U>& operator [] (const int &i) { return a[i]; }
+  void I(const int &_n) {
+    n = m = _n;
+    a = vector<vector<U>>(n, vector<U>(n, 0));
+    for (int i = 0; i < n; ++i) a[i][i] = 1;
+  }
+  Martix T() const {
+    Martix t(m, n);
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < m; ++j)
+        t[j][i] = a[i][j];
+    return t;
+  }
+  Martix& operator *=(const Martix &mx) { return *this = *this*mx; }
+  friend Martix operator *(const Martix &m1, const Martix &m2) {
+    assert(m1.m == m2.n);
+    Martix res(m1.n, m2.m);
+    for (int i = 0; i < res.n; ++i)
+      for (int j = 0; j < res.m; ++j)
+        for (int k = 0; k < m1.m; ++k)
+          res[i][j] += m1.a[i][k]*m2.a[k][j]; // MOD
+    return res;
+  }
+  template <typename TT>
+  Martix pow(const TT &p) const {
+    assert(n == m);
+    Martix res(n, m), a = *this;
+    res.I(n);
+    for (TT i = p; i; i >>= 1, a *= a) if (i&1) res *= a;
+    return res;
+  }
+  friend Martix inv(const Martix &m) { return m.inv(); }
+  Martix inv() const {
+    assert(n == m);
+    Martix res = *this;
+    vector<int> is(n, -1), js(n, -1);
+    for (int k = 0; k < n; ++k) {
+      for (int i = k; i < n; ++i)
+        for (int j = k; j < n; ++j) if (res[i][j]) {
+          is[k] = i; js[k] = j; break;
+        }
+      if (is[k] == -1) return Martix(0);
+      swap(res[k], res[is[k]]);
+      // for (int i = 0; i < n; ++i) swap(res[k][i], res[is[k]][i]);
+      for (int i = 0; i < n; ++i) swap(res[i][k], res[i][js[k]]);
+      // res[k][k] = mul_inverse(res[k][k]); // get inv of number
+      res[k][k] = 1.0/res[k][k];
+      for (int j = 0; j < n; ++j) if (j != k)
+        res[k][j] = res[k][j]*res[k][k];
+      for (int i = 0; i < n; ++i) if (i != k)
+        for (int j = 0; j < n; ++j) if (j != k)
+          res[i][j] -= res[i][k]*res[k][j];
+      for (int i = 0; i < n; ++i) if (i != k)
+        res[i][k] = -res[i][k]*res[k][k];
+    }
+    for (int k = n-1; k >= 0; --k) {
+      swap(res[k], res[js[k]]);
+      // for (int i = 0; i < n; ++i) swap(res[js[k]][i], res[k][i]);
+      for (int i = 0; i < n; ++i) swap(res[i][is[k]], res[i][k]);
+    }
+    return res;
+  }
+  friend ostream& operator <<(ostream &os, const Martix &mx) {
+    for (int i = 0; i < mx.n; ++i)
+      for (int j = 0; j < mx.m; ++j)
+        os << mx[i][j] << " \n"[j==mx.m-1];
+    return os;
+  }
+ private:
+  int n, m;
+  vector<vector<U>> a;
+};
+
 int dataNum;               //样本数目
 double attri[13][N], price[N], min_in[13], max_in[13], min_out, max_out, r[13];
-double w[14]; // w = (X'X)^(-1)X'y
+Martix<double> w; // w = (X'X)^(-1)X'y
 int choose[13];
 string Attribute_name[14];
 // "CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV"
@@ -61,11 +142,23 @@ double Correlation_coefficient(double x[], double y[]) {
 
 //线性回归
 void LineReg() {
-  ;
+  // w = (X'X)^(-1)X'y
+  Martix<double> X(dataNum, 5), y(dataNum, 1);
+  for (int i = 0; i < dataNum; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      X[i][j] = attri[i][choose[j]];
+    }
+    X[i][4] = 1;
+    y[i][0] = price[i];
+  }
+  w = inv(X.T()*X)*X.T()*y;
 }
 
-double Predict(double x[13]) {
-  return x[0]+x[1];
+double Predict(double inx[4]) {
+  Martix<double> x(5, 1);
+  for (int i = 0; i < 4; ++i) x[i][0] = inx[i];
+  x[0][4] = 1;
+  return (w*x)[0][0];
 }
 
 //输出
@@ -89,7 +182,7 @@ int main() {
   // 挑选四个最相关的维度
   iota(choose, choose+13, 0);
   sort(choose, choose+13, [&](int x, int y) {
-    return r[i] > r[y];
+    return r[x] > r[y];
   });
   // 多元线性回归
   LineReg();
