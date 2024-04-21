8678720bb0a5b6f3c56773d00d9e7fe8a92cc3ac
Kaizyn <2291443901@qq.com>
2024-04-22 00:20:44 +0800

    comp5212: second edition

--- COMP5212/DeepLearningGrading/model.py
+++ COMP5212/DeepLearningGrading/model.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset
 import numpy as np
+import pandas as pd
+import torch.optim as optim
+from tqdm import tqdm
+from sklearn.model_selection import KFold
 '''
 You need to implement the Net class and the dataset class for the model to work.
 
@@ -33,4 +37,154 @@ class dataset(Dataset):
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
-    
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(7, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = x.view(-1, 7)
+        return self.fc(x)
+
+# sample test set, just same shape
+class dataset(Dataset):
+    def __init__(self, file_path = '../data/validation.csv'):
+        data = pd.read_csv(file_path)
+        self.X = torch.from_numpy(data.drop('Label', axis=1).values).float()
+        self.y = torch.from_numpy(data[['Label']].values).float()
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+if __name__ == '__main__':
+    # # 创建数据集
+    # datas = dataset('../data/train.csv')
+
+    # # 定义K-fold交叉验证
+    # kfold = KFold(n_splits=5, shuffle=True)
+
+    # # 初始化最小验证损失为无穷大
+    # min_val_loss = float('inf')
+
+    # # 开始K-fold交叉验证
+    # for fold, (train_ids, val_ids) in tqdm(enumerate(kfold.split(datas)), desc="fold: "):
+    #     # 创建训练集和验证集数据加载器
+    #     train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+    #     val_subsampler = torch.utils.data.SubsetRandomSampler(val_ids)
+
+    #     train_loader = torch.utils.data.DataLoader(datas, batch_size=32, sampler=train_subsampler)
+    #     val_loader = torch.utils.data.DataLoader(datas, batch_size=32, sampler=val_subsampler)
+
+    #     # 创建网络和优化器
+    #     net = Net()
+    #     criterion = nn.BCEWithLogitsLoss()
+    #     optimizer = optim.Adam(net.parameters(), lr=0.01)
+
+    #     # 训练网络
+    #     for epoch in tqdm(range(10), desc="epoch: "):
+    #         running_loss = 0.0
+    #         for i, data in enumerate(train_loader, 0):
+    #             inputs, labels = data
+
+    #             # 清零梯度
+    #             optimizer.zero_grad()
+
+    #             # 前向传播，反向传播，优化
+    #             outputs = net(inputs)
+    #             loss = criterion(outputs, labels.view(-1, 1))
+    #             loss.backward()
+    #             optimizer.step()
+
+    #             # 打印统计信息
+    #             running_loss += loss.item()
+    #             if i % 20 == 19:  # 每2000个mini-batches打印一次
+    #                 print('[%d, %3d] loss: %.3f' %
+    #                       (epoch + 1, i + 1, running_loss / 2000))
+    #                 running_loss = 0.0
+
+    #     # 在验证集上计算损失
+    #     val_loss = 0.0
+    #     with torch.no_grad():
+    #         for data in val_loader:
+    #             inputs, labels = data
+    #             outputs = net(inputs)
+    #             loss = criterion(outputs, labels.view(-1, 1))
+    #             val_loss += loss.item()
+
+    #     print(f'Fold {fold}, Validation loss {val_loss}')
+
+    #     # 如果当前模型的验证损失比之前的最小验证损失还小，那么保存当前模型
+    #     if val_loss < min_val_loss:
+    #         min_val_loss = val_loss
+    #         torch.save(net.state_dict(), './final_model.pth')
+
+    # 创建数据集
+    train_dataset = dataset('../data/train.csv')
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+    # 创建网络和优化器
+    net = Net()
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.01)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+
+    # 创建验证集数据加载器
+    val_dataset = dataset('../data/validation.csv')
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=320, shuffle=True)
+
+    # 训练网络
+    best_loss = float('inf')
+    for epoch in tqdm(range(20), desc='epoch'):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+
+            # 清零梯度
+            optimizer.zero_grad()
+
+            # 前向传播，反向传播，优化
+            outputs = net(inputs)
+            loss = criterion(outputs, labels.view(-1, 1))
+            loss.backward()
+            optimizer.step()
+
+            # 打印统计信息
+            running_loss += loss.item()
+            if i % 20 == 19:  # 每200个mini-batches打印一次
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+        # 在验证集上计算损失
+        val_loss = 0.0
+        with torch.no_grad():
+            for data in val_loader:
+                inputs, labels = data
+                outputs = net(inputs)
+                loss = criterion(outputs, labels.view(-1, 1))
+                val_loss += loss.item()
+
+        # 如果验证损失下降，保存模型
+        if val_loss < best_loss:
+            best_loss = val_loss
+            torch.save(net.state_dict(), './final_model.pth')
+
+        # 更新学习率
+        scheduler.step(val_loss)
+
+    print('Finished Training')
--- COMP5212/MachinLearningGrading/mlmodel.py
+++ COMP5212/MachinLearningGrading/mlmodel.py
@@ -1,32 +1,103 @@
+'''
+Author: Kaizyn 2291443901@qq.com
+Date: 2024-04-14 23:55:12
+LastEditors: Kaizyn 2291443901@qq.com
+LastEditTime: 2024-04-21 23:52:09
+FilePath: \COMP5212\MachinLearningGrading\mlmodel.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+# from xgboost import XGBClassifier
+# from imblearn.over_sampling import SMOTE, ADASYN
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-# from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 
 import pandas as pd
 
 
-# Here is only a template for your reference, you only need to ensure the predict function can 
+# Here is only a template for your reference, you only need to ensure the predict function can
 # receive the test dataset and return the prediction results.
 
 
-
-class MachineLearningModel():
+class MachineLearningModel:
     def __init__(self):
-        self.model = SVC(kernel='rbf', C=0.5, gamma='auto')
+        self.model = SVC(kernel="rbf", C=0.5, gamma="auto")
+
+        df_train = pd.read_csv("../data/train.csv")
+        self.train(df_train)
 
     def train(self, data: pd.DataFrame):
-        #you can do your training here
-        self.model = self.model.fit(data.drop('Label', axis=1), data['Label'])
+        # you can do your training here
+        self.model = self.model.fit(data.drop("Label", axis=1), data["Label"])
 
     # def preprocess(self, data):
-        # you can do your preprocessing here
+    # you can do your preprocessing here
 
-    def predict(self, data):
-        df_train = pd.read_csv('./train.csv')
+    def predict(self, data: pd.DataFrame):
         # Apply any data preprocessing if you want, just to keep the test data in the same format as the training data
+        return self.model.predict(data)
+
+class SVCModel:
+    def __init__(self):
+        self.model = SVC(kernel="rbf", C=0.5, gamma="auto")
+
+        df_train = pd.read_csv("../data/train.csv")
         self.train(df_train)
+
+    def train(self, data: pd.DataFrame):
+        self.model = self.model.fit(data.drop("Label", axis=1), data["Label"])
+
+    def predict(self, data: pd.DataFrame):
+        return self.model.predict(data)
+
+
+class PCAModel:
+    def __init__(self):
+        self.model = SVC(kernel="rbf", C=0.5, gamma="auto")
+        self.scaler = StandardScaler()
+        self.pca = PCA(n_components=4)
+
+        df_train = pd.read_csv("../data/train.csv")
+        self.train(df_train)
+
+    def train(self, data: pd.DataFrame):
+        data_scaled = self.scaler.fit_transform(data.drop("Label", axis=1))
+        data_pca = self.pca.fit_transform(data_scaled)
+        self.model = self.model.fit(data_pca, data["Label"])
+
+    def predict(self, data: pd.DataFrame):
+        data_scaled = self.scaler.transform(data)
+        data_pca = self.pca.transform(data_scaled)
+        return self.model.predict(data_pca)
+
+
+# class EnsembleModel:
+class MachineLearningModel:
+    def __init__(self):
+        # 创建多个模型
+        # clf1 = XGBClassifier()
+        clf2 = RandomForestClassifier(n_estimators=50, random_state=1)
+        clf3 = SVC(kernel="rbf", C=0.5, gamma="auto", probability=True)
+        clf4 = KNeighborsClassifier()
+
+        # 创建 VotingClassifier
+        self.model = VotingClassifier(estimators=[
+            # ("xgb", clf1), 
+            ("rf", clf2), 
+            ("svc", clf3), 
+            ("knn", clf4)
+        ], voting="soft")
+
+        df_train = pd.read_csv("../data/train.csv")
+        self.train(df_train)
+
+    def train(self, data: pd.DataFrame):
+        self.model = self.model.fit(data.drop("Label", axis=1), data["Label"])
+
+    def predict(self, data: pd.DataFrame):
         return self.model.predict(data)
